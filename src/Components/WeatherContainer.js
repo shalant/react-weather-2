d976f31ecfc7892de2938139c5171cf5c968f2d8
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-21 22:25:45 -0600

    got a bug with API, it works in console log but not quite in the components section 3922

--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../Styles/Weather.css'
 
 function WeatherContainer () {
+    const API_KEY = 'b04b2a052fb4b71af764d1f2a6d8f3ab';
     const [searchQuery, setSearchQuery] = useState('');
     const [weatherData, setWeatherData] = useState({
         temp: null,
@@ -20,13 +21,27 @@ function WeatherContainer () {
             setIsValidZipCode(true);
         } else {
             setIsValidZipCode(false);
-
         }
     }
 
     function validateZipCode(zipCode) {
         let regex = /[0-9]{5}/;
         return regex.test(zipCode);
+    }
+
+    function getWeatherData() {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${searchQuery},us&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData({
+            temp: convertToFarenheit(data.main.temp),
+            humidity: data.main.humidity,
+            desc: data.weather[0].main,
+            city: data.name
+        }));
+    }
+
+    function convertToFarenheit(temp) {
+        return (temp - 273.15) * (9.0 / 5.0) + 32;
     }
 
     return (
@@ -40,7 +55,9 @@ function WeatherContainer () {
                         onChange={updateSearchQuery}
                         maxLength='5'
                     />
-                    <button className='material-icons'>search</button>
+                    <button 
+                        onClick={getWeatherData}
+                        className='material-icons'>search</button>
                 </div>
             </header>
             <p className='error'>{isValidZipCode ? '' : 'Invalid Zip Code'}</p>
