6bc6d5930766d179d094da7369dce36e154af516
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-23 21:41:37 -0600

    added custom css to center the widget, 10029

--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -5,22 +5,21 @@ function WeatherInfo(props) {
     const { temp, humidity, desc, city } = props.data;
     return (
         <React.Fragment>
-            <h3>{desc}</h3>
-            <div className='header-description' >
-                <h4>City</h4>
-                <p>{city}</p>
-            </div>
-            <div className='header-description' >
-                <h4>Temperature</h4>
-                <p>{temp}</p>
-            </div>
-            <div className='header-description' >
-                <h4>Humidity</h4>
-                <p>{humidity}</p>
-            </div>
-
-            
-            
+            <h2>{desc}</h2>
+            <section className='weather-data-flex'>
+                <div className='header-description' >
+                    <h4>City</h4>
+                    <p>{city}</p>
+                </div>
+                <div className='header-description' >
+                    <h4>Temperature</h4>
+                    <p>{temp}<span className='degree-symbol'></span>F</p>
+                </div>
+                <div className='header-description' >
+                    <h4>Humidity</h4>
+                    <p>{humidity}%</p>
+                </div>
+            </section>
             
         </React.Fragment>
     );
